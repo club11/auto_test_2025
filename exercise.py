3af4b9cdf8bf5efe4ca6ed200c9d187414bbbb90
club11 <club11@bk.ru>
2025-02-09 02:08:58 +0300

    second homework with russian

--- exercise.py
+++ exercise.py
@@ -1,24 +1,11 @@
-'''
-Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
-Напишите программу, которая добавляет ‘ing’ к словам
-В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
-Напишите программу которая удаляет пробел в начале, в конце строки
-Имена собственные всегда начинаются с заглавной буквы, за которой следуют строчные буквы. Исправьте данное имя собственное так, чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris"
-Перевести строку в список "Robin Singh" => ["Robin”, “Singh"]
-"I love arrays they are my favorite" => ["I", "love", "arrays", "they", "are", "my", "favorite"]
-Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus
-Напечатайте текст: “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
-Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"] сделайте из него строку => "I love arrays they are my favorite"
-Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение, удалите элемент из списка под индексом 6
-После выполнения домашнего задания, зафиксируйте свои изменения и сделайте PR, так же как вы это делали в ДЗ по Git
-'''
 
-# 1. Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
+#
+#1. 'www.my_site.com#about' =>  'www.my_site.com/about'
 origin_string = 'www.my_site.com#about'
 enhanced_string = origin_string.replace('#', '/')
-print(enhanced_string, '!!!!!')
+print(enhanced_string)
 
-# 2. Напишите программу, которая добавляет ‘ing’ к словам
+# 2. ‘ing’
 def add_ing():
     print("Enter a word or a sentence to 'ing' modify:")
     given_data = input()
@@ -29,13 +16,13 @@ def add_ing():
 
 add_ing()
 
-#3. В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
-ivanov_str = 'Ivanou Ivan'
+#3. Convert
+ivanov_str = 'Ivanov Ivan'
 lst_name, name = ivanov_str.split(' ')
 ivanov_str_new = name + ' ' + lst_name
 print(ivanov_str_new)
 
-#4. Напишите программу которая удаляет пробел в начале, в конце строки
+#4. Delete a space at the beginning and end of the line.
 def del_space_in_string():
     print("Enter a word or a sentence to delete spaces:")
     given_data = input()
@@ -46,12 +33,12 @@ def del_space_in_string():
 
 del_space_in_string()
 
-#5. Имена собственные всегда начинаются с заглавной буквы, за которой следуют строчные буквы. Исправьте данное имя собственное так, чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris"
+#5. "pARiS" >> "Paris"
 wrong_name = "pARiS"
 corrected_name = wrong_name.capitalize()
 print(corrected_name)
 
-#6. Перевести строку в список "Robin Singh" => ["Robin”, “Singh"]
+#6.  "Robin Singh" => ['Robin', 'Singh']
 #6.1. "I love arrays they are my favorite" => ["I", "love", "arrays", "they", "are", "my", "favorite"]
 robin_string = "Robin Singh"
 robin_list = list(robin_string.split(' '))
@@ -61,18 +48,18 @@ array_string = "I love arrays they are my favorite"
 array_list = list(array_string.split(' '))
 print(array_list)
 
-#7. Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus. Напечатайте текст: “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
-ivan_list = ['Ivan', 'Ivanou']
+#7. [Ivan, Ivano],  Minsk, Belarus >>
+ivan_list = ['Ivan', 'Ivanov']
 minsk_str = 'Minsk'
 belarus_str = 'Belarus'
 print(f'Привет, {ivan_list[0]} {ivan_list[1]}! Добро пожаловать в {minsk_str} {belarus_str}')
 
-#8. Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"] сделайте из него строку => "I love arrays they are my favorite"
+#8. ["I", "love", "arrays", "they", "are", "my", "favorite"]  => "I love arrays they are my favorite"
 list_given = ["I", "love", "arrays", "they", "are", "my", "favorite"]
 str_got = ' '.join(str(x) for x in list_given)
 print(str_got)
 
-#9. Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение, удалите элемент из списка под индексом 6
+#9. Create a list of 10 elements, insert a new value at the 3rd position, remove the element from the list at index 6
 ten_elements_list = ['съешь', 'же', 'ещё', 'этих', 'мягких', 'французских', 'булок', 'да', 'выпей', 'чаю']
 print(len(ten_elements_list))
 ten_elements_list.insert(3, 'Император защищает!')
