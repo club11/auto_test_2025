ca05ce6c9fa734153125863c017044b8d2d657e4
club11 <club11@bk.ru>
2025-03-18 21:49:51 +0300

    Test: <Wlad Filippow

---
+++ test/task2.py
@@ -0,0 +1,18 @@
+""" Task 2"""
+
+def square_func():
+    """
+     запрашивает у пользователя число и выводит его квадр
+    :return:
+    """
+    get_data = input('enter some value:')
+    return int(get_data)  ** 2
+
+def even_func():
+    """
+    определяет, является ли оно четным или нечетным
+    """
+    get_even_data = input('enter some value:')
+    if int(get_even_data) % 2 == 0:
+        return print('EVEN')
+    return print('NOT EVEN')
