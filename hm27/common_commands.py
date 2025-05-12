957b089db168d83ac07f60b39d9dec28fe46437f
club11 <club11@bk.ru>
2025-05-12 19:17:58 +0300

    hm27 common request func added2

--- hm27/common_commands.py
+++ hm27/common_commands.py
@@ -1,7 +1,6 @@
 """A helper file with common func"""
 
 import requests
-from hm28.API.conf_data import Conf, RequiredData
 
 
 def make_request(request_method, an_url, a_data=None, a_header=None, a_timeout=5):
