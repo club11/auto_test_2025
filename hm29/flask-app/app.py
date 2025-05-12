1410ddbf761838d67cdd244ff0dd0c8dc9ea153f
club11 <club11@bk.ru>
2025-05-13 00:09:10 +0300

    hm29 Docker linter fixed

--- hm29/flask-app/app.py
+++ hm29/flask-app/app.py
@@ -12,11 +12,13 @@ images = [
     "https://media.tenor.com/lQlIBQeeruwAAAAd/wanted-cat.gif"
     ]
 
+
 @app.route('/')
 def index():
     """render template"""
     url = random.choice(images)
     return render_template('index.html', url=url)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8866")
